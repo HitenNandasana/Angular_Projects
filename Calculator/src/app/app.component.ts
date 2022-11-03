04d7827e17452e44c645bf0d4e31f1ad8f316a8d
Hiten <hitennandasana2001@gmail.com>
2022-11-03 17:38:06 +0530

    cal app changes in fix some bug for 2nd commit

--- Calculator/src/app/app.component.ts
+++ Calculator/src/app/app.component.ts
@@ -7,21 +7,89 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
-  digit = '';
+  input = '';
+  answer = false;
 
   fun(number: any) {
-    this.digit += number;
+    if (this.input.length < 15) {
+      if (this.answer) {
+        if (this.input !== '') {
+          this.input = '';
+          this.answer = false;
+        }
+      }
+      let lastChar = this.input.charAt(this.input.length - 1);
+      let pos = this.getLastOperandPosition();
+      if (number === '0') {
+        if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/' || lastChar === '%') {
+          this.input += number;
+        }
+        else if (this.input.charAt(pos + 1) !== '0') {
+          this.input += number;
+        }
+        if (this.input === '') {
+          this.input += number;
+        }
+      } else {
+        if (this.input.charAt(pos + 1) !== '0') {
+          this.input += number;
+        }
+        else {
+          this.input = this.input.replace(/.$/, number);
+        }
+      }
+    }
   }
+
   clear() {
-    this.digit = ''
+    this.input = '';
+    this.answer = false;
   }
   negative() {
+    let lastChar = this.input.charAt(this.input.length - 1);
+    let pos = this.getLastOperandPosition();
+    console.log(pos);
+    if (this.input !== '') {
+      let i = this.input.charAt(pos + 1);
+    } else {
 
+    }
   }
-  modulo() {
 
+  operatorFun(operator: any) {
+    if (this.input !== '') {
+      this.answer = false;
+      let lastChar = this.input.charAt(this.input.length - 1);
+      if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/' || lastChar === '%') {
+        if (lastChar !== operator) {
+          this.input = this.input.replace(/.$/, operator);
+        }
+      } else {
+        this.input += operator;
+      }
+    }
   }
+
   equal() {
-    this.digit = eval(this.digit);
+    if (this.answer) {
+      return;
+    } else {
+      let lastChar = this.input.charAt(this.input.length - 1);
+      if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/' || lastChar === '%') {
+        this.input = this.input.replace(/.$/, '');
+      }
+      this.input = eval(this.input).toString();
+      this.answer = true;
+    }
+  }
+
+  getLastOperandPosition() {
+    let pos: number;
+    pos = this.input.lastIndexOf('+');
+    if (this.input.lastIndexOf('-') > pos) pos = this.input.lastIndexOf('-');
+    if (this.input.lastIndexOf('*') > pos) pos = this.input.lastIndexOf('*');
+    if (this.input.lastIndexOf('/') > pos) pos = this.input.lastIndexOf('/');
+    if (this.input.lastIndexOf('%') > pos) pos = this.input.lastIndexOf('%');
+    return pos;
   }
 }
